ca90f3c825a251d0407b900f3897f1c69b1e2b2b
Praveen Kumaravel <praveen1994kp@gmail.com>
2019-11-30 01:39:47 +0530

    Upload service and list files

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
 import express from 'express'
+import multer from 'multer'
+import fs from 'fs'
+import cors from 'cors'
+
+const uploadPath = './uploads'
+
 
 const server = express()
+server.use(cors())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, uploadPath)
+    },
+    filename: function (req, file, callback) {
+        console.log(file)
+        callback(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage });
 
 
 server.get('/', (req, res) => {
     res.send('OK')
+})
+
+server.get('/files', async (req, res) => {
+    let files = []
+    fs.readdirSync(uploadPath).forEach(file => {
+        console.log(file);
+        files.push(file)
+      });
+
+      res.json(files)
+})
+
+server.post('/file', upload.single('testFile'), (req, res) => {
+    const file = req.file
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+
+    res.send(file)
 })
 
 server.listen(8080, () => console.log('Listening on 8080'))
